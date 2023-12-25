6cdec820850a78034ffade773ece18319268c0dd
Yahya Haliloğlu <halilogluyahya@gmail.com>
2023-12-25 09:55:37 +0300

    Hastalıklar
    hastalık türleri ve hastalık sayfaları eklendi

---
+++ HastaneApp/HastaneApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using HastaneApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HastaneApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Love()
+        {
+            return View();// bu durumda iken love adlı cshtml dosyasını çalıştırır.
+            //return View("Test") -> yazdığımızda Test.cshtml adlı Home dosyası altında ki view ı açar.
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HastaneApp/HastaneApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HastaneApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
