cb2438b4ead855603b52efefdb28b166fb7cf29a
Yahya Haliloğlu <halilogluyahya@gmail.com>
2023-12-26 11:34:47 +0300

    Doktor Sayfası

---
+++ Hospilom/Hospilom/Controllers/DoktorTuruController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hospilom.Controllers
+{
+    public class DoktorTuruController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
