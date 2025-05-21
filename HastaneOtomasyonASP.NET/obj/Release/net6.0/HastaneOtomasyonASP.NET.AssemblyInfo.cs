7b4ebd30ecba02b9a7d9baccddf08ee39ecae3b9
Yahya Haliloğlu <halilogluyahya@gmail.com>
2025-05-21 10:41:58 +0300

    update

--- HastaneOtomasyonASP.NET/obj/Release/net6.0/HastaneOtomasyonASP.NET.AssemblyInfo.cs
+++ HastaneOtomasyonASP.NET/obj/Release/net6.0/HastaneOtomasyonASP.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HastaneOtomasyonASP.NET")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4857083080921369e7dd952a1cc5062883ab4e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("HastaneOtomasyonASP.NET")]
